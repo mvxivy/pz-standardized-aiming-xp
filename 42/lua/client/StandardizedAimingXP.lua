73a2aa6f66bf41dcc1a632013891dbbc806dddfd
Maksim Ivanov <vonavimax@gmail.com>
2025-01-23 22:51:41 +0700

    feat: 42b mod structure

---
+++ 42/lua/client/StandardizedAimingXP.lua
@@ -0,0 +1,41 @@
+--****************************************************************************************************************
+--**		Created By: 	Conqueror Koala																		**
+--**		Updated By:		Mortazar																					**
+--** 		Last update at: 	23-01-2025																		**
+--****************************************************************************************************************
+--**		Mod:			Standardized Aiming XP																**
+--**																											**		
+--**		Information:																						**
+--**					This code is totally free for you to edit, use, or copy however you want!				**
+--**					Feel free to use any of the code in your own projects, don't worry about crediting. 	**
+--****************************************************************************************************************
+
+--************************************************************************************************
+--**  Would not recommend changing anything past this point unless you know what you are doing. **
+--************************************************************************************************
+local SETTINGS = StandardizedAimingXP_global.SETTINGS
+
+local aimingMult = {1,1.5,2,3,5,10};
+--					(1x,1.5x,2x,3x,5x,10x) -- The REAL xp multipliers that effect your game!
+
+
+onGunHitXp = function(owner, weapon, hitObject, damage)
+	-- add xp for ranged weapon
+	
+	if weapon:isRanged() then
+		local origXP = owner:getLastHitCount();
+		local xp = origXP;
+		if owner:getPerkLevel(Perks.Aiming) >= 5 then
+			xp = origXP * 2.7 * tonumber(aimingMult[SETTINGS.options.dropdown1]);
+			xp = xp - origXP;
+			owner:getXp():AddXP(Perks.Aiming, xp);
+		end
+		if (owner:getPerkLevel(Perks.Aiming) < 5) and (tonumber(aimingMult[SETTINGS.options.dropdown1]) ~= 1) then
+			xp = origXP * 2.7 * tonumber(aimingMult[SETTINGS.options.dropdown1]);
+			xp = xp - (origXP * 2.7);
+			owner:getXp():AddXP(Perks.Aiming, xp);
+		end
+	end
+end
+
+Events.OnWeaponHitXp.Add(onGunHitXp)
